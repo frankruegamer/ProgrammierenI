2a8de35e44642f338c192945e6f51353402a4cb4
Frank <mail@frankruegamer.de>
2015-11-02 23:04:00 +0100

    Changed "sleep" method to lambda expression

--- Übung 4/src/FreierFall.java
+++ Übung 4/src/FreierFall.java
@@ -20,16 +20,17 @@ public class FreierFall {
 
     public static void freierFall(int t) {
         Consumer<Integer> sleep = s -> {
-            try { Thread.sleep(s * 1000);}
+            try   {Thread.sleep(s * 1000);}
             catch (InterruptedException e) { }
         };
 
+        // The number of steps between calculated heights
         final int step = 5;
 
         final double g = 9.80665;
         sleep.accept(1);
         for (int i = 1; i <= t; i += step) {
-            double s = (1./2) * g * i * i;
+            double s = (1./2) * g * i*i;
             System.out.printf("Zeit: %d Sekunden: Zurückgelegte Strecke: %.4f m%n", i, s);
             if (i <= t - step) sleep.accept(step);
         }
