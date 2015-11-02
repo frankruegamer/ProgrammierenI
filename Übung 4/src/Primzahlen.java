1a7fed9dc22060400f312ea07a4f4b2cd4900e47
Frank <mail@frankruegamer.de>
2015-11-02 22:15:42 +0100

    Added Übung 4, Task 4 (Primzahlen)

---
+++ Übung 4/src/Primzahlen.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Frank on 02.11.2015.
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Primzahlen {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bitte die untersuchende Zahl eingeben: ");
+        try {
+            if (primzahl(sc.nextInt())) {
+                System.out.println("Untersuchte Zahl ist eine Primzahl !");
+            } else {
+                System.out.println("Untersuchte Zahl ist keine Primzahl !");
+            }
+        } catch (InputMismatchException e) {
+        }
+    }
+
+    public static boolean primzahl(int zahl) {
+        boolean prime = true;
+        for (int i = 2; i <= Math.sqrt(zahl); i++) {
+            if (zahl % i == 0) prime = false;
+        }
+        return prime;
+    }
+}
