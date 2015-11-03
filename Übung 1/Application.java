d16576a0b5b1d1f7cfe9106062c08ed50afbc475
Frank <mail@frankruegamer.de>
2015-11-03 01:12:00 +0100

    Removed a batch and moved a class

--- Übung 1/Application.java
+++
@@ -1,5 +0,0 @@
-public class Application {
-	public static void main (String[] args) {
-		System.out.println("Willkommen zu Programmieren1");
-	}
-}http://marketplace.eclipse.org/marketplace-client-intro?mpc_install=1403812
--- Übung 1/Aufgabe3.java
+++
@@ -1,5 +0,0 @@
-public class Aufgabe3 {
-	public static void main (String[] args) {
-		System.out.println(3 * (9 + 3) + 4 * 8);
-	}
-}
