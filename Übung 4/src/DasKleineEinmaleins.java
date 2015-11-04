0e4445f36a44f04aac71ebf9a153a8a12d953c27
Frank <mail@frankruegamer.de>
2015-11-04 20:49:10 +0100

    Renamed method from "dasKleineEinmalEins" to "dasKleineEinmaleins"

--- Übung 4/src/DasKleineEinmaleins.java
+++ Übung 4/src/DasKleineEinmaleins.java
@@ -1,13 +1,14 @@
 /**
  * Created by Frank on 02.11.2015.
  */
+
 public class DasKleineEinmaleins {
 
     public static void main(String[] args) {
-        dasKleineEinmalEins();
+        dasKleineEinmaleins();
     }
 
-    public static void dasKleineEinmalEins() {
+    public static void dasKleineEinmaleins() {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 System.out.printf("%d x %d = %d%n", i, j, i * j);
