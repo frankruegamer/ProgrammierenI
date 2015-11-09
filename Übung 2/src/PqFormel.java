ec6f27e343500df9d14402c6f79245c1d3dfb525
Frank <mail@frankruegamer.de>
2015-11-09 17:56:00 +0100

    Added Übung 5 Task 5 (Temperaturtabelle)

--- Übung 2/src/PqFormel.java
+++ Übung 2/src/PqFormel.java
@@ -5,13 +5,16 @@ public class PqFormel {
 		double p = 4;
 		double q = 4;
 		double[] pq = pqFormel(p, q);
+		if (pq == null) return;
 		System.out.printf("x1: %.3f; x2: %.3f", pq[0], pq[1]);
 	}
 
 	public static double[] pqFormel (double p, double q) {
 		double[] rueckgabe = new double[2];
 		double f1 = -(p/2);
-		double f2 = Math.sqrt(f1*f1-q);
+		double square = f1*f1-q;
+		if (square < 0) return null;
+		double f2 = Math.sqrt(square);
 		rueckgabe[0] = f1 - f2;
 		rueckgabe[1] = f1 + f2;
 		return rueckgabe;
---
+++ Übung 5/src/Temperaturtabelle.java
@@ -0,0 +1,15 @@
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Created by Frank on 09.11.2015.
+ */
+public class Temperaturtabelle {
+
+    public static void main(String[] args) {
+        DoubleUnaryOperator degreeFtoC = f -> (5./9) * (f-32);
+        System.out.println("Fahrenheit\tCelsius");
+        for (int f = -1 + 1; f <= 300; f++)
+            System.out.printf("%-10d\t%.2f%n", f, degreeFtoC.applyAsDouble(f));
+    }
+
+}
