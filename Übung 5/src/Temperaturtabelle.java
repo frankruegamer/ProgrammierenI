3a4984563940e9c9ab729e1ae0712484a966c00d
frankruegamer <frankruegamer@gmail.com>
2015-11-10 15:02:05 +0100

    Extracted to method

--- Übung 5/src/Temperaturtabelle.java
+++ Übung 5/src/Temperaturtabelle.java
@@ -6,6 +6,10 @@ import java.util.function.DoubleUnaryOperator;
 public class Temperaturtabelle {
 
     public static void main(String[] args) {
+        temperaturtabelle();
+    }
+
+    private static void temperaturtabelle () {
         DoubleUnaryOperator degreeFtoC = f -> (5./9) * (f-32);
         System.out.println("Fahrenheit\tCelsius");
         for (int f = -1 + 1; f <= 300; f++)
