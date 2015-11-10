818720684bf4545540f611a40f18f66a7fd1491c
frankruegamer <frankruegamer@gmail.com>
2015-11-10 14:23:12 +0100

    Added Übung 5 Task 2 (Querprodukt)

---
+++ Übung 5/src/Querprodukt.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by Frank on 09.11.2015.
+ */
+public class Querprodukt {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Zahl ∈ [0; 1,000,000]:");
+            try {
+                int zahl = sc.nextInt();
+                if (zahl < 0 || zahl > 1000000) throw new InputMismatchException();
+                System.out.printf("Q(%d) = %d", zahl, querprodukt(zahl));
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Geben Sie eine ganze Zahl zwischen 0 und 1 Mio. ein!\n");
+            }
+        }
+    }
+
+    public static int querprodukt (int zahl) {
+        int produkt = 1;
+        while (zahl != 0) {
+            produkt *= (zahl % 10);
+            zahl /= 10;
+        }
+        return produkt;
+    }
+
+
+}
