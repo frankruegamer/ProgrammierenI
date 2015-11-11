54a1f80f9646dfb0e562bcf40ad781198c1a8d72
frankruegamer <frankruegamer@gmail.com>
2015-11-11 08:45:37 +0100

    Removed unnecessary operation

--- Übung 5/src/Temperaturtabelle.java
+++ Übung 5/src/Temperaturtabelle.java
@@ -12,7 +12,7 @@ public class Temperaturtabelle {
     public static void temperaturtabelle() {
         DoubleUnaryOperator degreeFtoC = f -> (5./9) * (f-32);
         System.out.println("Fahrenheit\tCelsius");
-        for (int f = -1 + 1; f <= 300; f++)
+        for (int f = 0; f <= 300; f++)
             System.out.printf("%-10d\t%.2f%n", f, degreeFtoC.applyAsDouble(f));
     }
 }
