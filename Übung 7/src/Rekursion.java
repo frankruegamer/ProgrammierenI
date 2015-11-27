c7fd08d7cdeb31b20533becc8f2cec578c3385cd
frankruegamer <frankruegamer@gmail.com>
2015-11-27 22:43:08 +0100

    Changed recursion to ternary statement + Formatting

--- Übung 7/src/Rekursion.java
+++ Übung 7/src/Rekursion.java
@@ -8,9 +8,9 @@ public class Rekursion {
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Geben Sie die Basis ein:");
+            System.out.print("Geben Sie die Basis ein: ");
             int basis = sc.nextInt();
-            System.out.println("Geben Sie den Exponenten ein:");
+            System.out.print("Geben Sie den Exponenten ein: ");
             int exponent = sc.nextInt();
             System.out.printf("%d hoch %d ist %d", basis, exponent, potenz(basis, exponent));
         } catch (InputMismatchException ignored) {
@@ -18,10 +18,6 @@ public class Rekursion {
     }
 
     public static int potenz (int x, int n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return x * potenz (x, n-1);
-        }
+        return n == 0 ? 1 : x * potenz (x, n-1);
     }
 }
