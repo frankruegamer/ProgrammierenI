eed1365671ed02b65b803097223ee041614c08f9
Frank <mail@frankruegamer.de>
2016-01-06 16:44:00 +0100

    Change first for loop to adapt counting scheme to second loop

--- Übung 10/src/Hochschule/Hochschule.java
+++ Übung 10/src/Hochschule/Hochschule.java
@@ -8,10 +8,10 @@ public class Hochschule {
     public static void main(String[] args) {
         Raum raum1 = new Raum("I.2.15", 20);
         Raum raum2 = new Raum("I.2.1", 20);
-        for (int i = 1; i <= 2; i++) {
+        for (int i = 0; i <= 1; i++) {
             System.out.println("Raum 1: " + raum1.istBelegt());
             System.out.println("Raum 2: " + raum2.istBelegt());
-            if (i == 1) raum1.belegen(new Vorlesung("Programmieren I", "Heinzl", "BIN"));
+            if (i == 0) raum1.belegen(new Vorlesung("Programmieren I", "Heinzl", "BIN"));
         }
         Student[] studenten = {
                 new Student("Peter"),
